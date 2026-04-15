9e2870ef8cd104440f35c93b713ebca38d7b5612
Akash Singh <2002akashsingh21@gmail.com>
2026-04-15 17:47:03 +0530

    Remove duplicate files

--- download_bert_model.py
+++
@@ -1,6 +0,0 @@
-from transformers import BertTokenizer, BertModel
-
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', cache_dir='./bert-base-uncased')
-model = BertModel.from_pretrained('bert-base-uncased', cache_dir='./bert-base-uncased')
-
-print("Downloaded BERT model and tokenizer locally to './bert-base-uncased'")
